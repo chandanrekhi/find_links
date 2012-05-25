00a67080841092bbcc9d88cc28e9b3c2cb296b37
Chandan Rekhi <er.chandanrekhi@gmail.com>
2012-05-25 12:32:54 +0530

    Code wrapped in a class

--- find_links.rb
+++ find_links.rb
@@ -3,39 +3,40 @@
 require 'rubygems'
 require 'twitter'
 
-
+class Findlinks
+  
 # Please check that the hash symbol is prefixed to a hashtag.
 
-def hashtag(tag_to_search)
-  (tag_to_search.scan(/^#/).empty?) ? "##{tag_to_search}" : tag_to_search
-end
+  def hashtag(tag_to_search)
+    (tag_to_search.scan(/^#/).empty?) ? "##{tag_to_search}" : tag_to_search
+  end
 
 # Search the tweets with the particular hashtag
-def tweets_search(tag_to_search)
-  Twitter.search(tag_to_search, :rpp => 100, result_type: 'recent')
-end
+  def tweets_search(tag_to_search)
+    Twitter.search(tag_to_search, :rpp => 100, result_type: 'recent')
+  end
 
 # Return a sorted list of unique links found in the list of tweets.
-def links(tweet_urls)
-  tweet_urls.map(&:text).grep( %r{http://}i ).to_s.scan( Regexp.new %r{http://[\w/.%-]+}i ).uniq
-end
+  def links(tweet_urls)
+    tweet_urls.map(&:text).grep( %r{http://}i ).to_s.scan( Regexp.new %r{http://[\w/.%-]+}i ).uniq
+  end
 
 # Displays message with correct format for appropriate usage of the application and does not let the application throw an exception.
-def implement
-  puts "How to use:-> ruby #{File.basename $0} aHashtag"
-  exit
-end
+  def implement
+    puts "How to use:-> ruby #{File.basename $0} aHashtag"
+    exit
+  end
 
-
+result = Findlinks.new
 if __FILE__ == $0
 begin
   implement unless ARGV.size >= 1
-  tag_to_search = hashtag(ARGV[0])
-  tweet_urls = tweets_search(tag_to_search)
-  puts links(tweet_urls)
+  tag_to_search = result.hashtag(ARGV[0])
+  tweet_urls = result.tweets_search(tag_to_search)
+  puts result.links(tweet_urls)
 rescue
 	puts "*** Please enter a valid hashtag option ***\n"
-	implement
 	exit
+end
 end
 end
