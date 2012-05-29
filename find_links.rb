e237424a309647be19e68ba4a64c086025e46964
Chandan Rekhi <er.chandanrekhi@gmail.com>
2012-05-29 18:06:18 +0530

    Minor changes

--- find_links.rb
+++ find_links.rb
@@ -13,14 +13,14 @@ class Findlinks
 
 # Search the tweets with the particular hashtag
   def tweets_search(tag_to_search)
-    Twitter.search(tag_to_search, :rpp => 100, result_type: 'recent')
+    Twitter.search(tag_to_search, :rpp => 100, :result_type => 'recent')
   end
 
 # Return a sorted list of unique links found in the list of tweets.
   def links(tweet_urls)
-    tweet_urls.map(&:text).grep( %r{http://}i ).to_s.scan( Regexp.new %r{http://[\w/.%-]+}i ).uniq
+    tweet_urls.map(&:text).grep( %r{http://}i ).to_s.scan( %r{http://[\w/.%-]+}i ). uniq
   end
-
+   
 # Displays message with correct format for appropriate usage of the application and does not let the application throw an exception.
   def implement
     puts "How to use:-> ruby #{File.basename $0} aHashtag"
